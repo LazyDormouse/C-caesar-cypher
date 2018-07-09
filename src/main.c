83fbacaea1ddf116764165c8674d03792af2beaf
PointyNeedle <agostinosaviano01@gmail.com>
2018-07-09 14:13:37 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,117 +10,82 @@
 #include <string.h>
 #include <ctype.h>
 
-void decrypt(char *string, int key);
-void encrypt(char *string, int key);
+int isletter(char c);
+char* encrypt_decrypt(char *string, int key, int encrypt);
 void usage();
 
 int main(int argc, char *argv[])
   {
     int k, i, eflag;
-    if (argc != 4)
-      usage();
+    // check if 4 arguments and correct encrypt/decrypt options were given
+    if ((argc != 4) || !((strcmp("-e", argv[1]) == 0) || (strcmp("-d", argv[1]) == 0)))
+      usage(argv[0]);
     else
       {
-        //checking if we're encrypting the message or not, assume not
+        //checking if we're encrypting the message or not
         if(strcmp("-e", argv[1]) == 0)
           eflag = 1;
         else if(strcmp("-d", argv[1]) == 0)
           eflag = 0;
-        else
-          {
-            puts("[WARNING] invalid option detected, the string will be decrypted");
-            eflag = 0;
-          }
         //allocating memory for the string
         char* message = strdup(argv[2]); //message is now the string we're going to operate on
         k = atoi(argv[3]);
-        //since I'm too lazy to deal with mixed case letters, we're converting
-        //the whole message to lowercase
+        // converting the whole message to lowercase
         for(i = 0; message[i]; i++)
             message[i] = tolower(message[i]);
         //if eflag is 1 encrypt the message, else decrypt it
-        if(eflag == 1)
-          encrypt(message, k);
-        else
-          decrypt(message, k);
+        puts(encrypt_decrypt(message, k, eflag));
         //done dealing with the message, let's free memory up
         free(message);
       }
     return 0;
   }
 
-void decrypt(char *string, int key)
+int isletter(char c)
   {
-    int i, j, a, string_lenght;
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-    string_lenght = strlen(string);
-    for(i = 0; i < string_lenght; i++)
+    int isletter = 0;
+    for(int i = 0; i < strlen(alphabet); i++)
       {
-        //checking if the char we're decrypting is a space
-        //if it is, leave it
-        if(!isspace(string[i]))
+        if(c == alphabet[i])
+          isletter = 1;
+      }
+    return isletter;
+  }
+
+char* encrypt_decrypt(char *string, int key, int encrypt)
+  {
+    int i, j, a;
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    for(i = 0; i < strlen(string); i++)
+      {
+        //checking if the char we're decrypting is a letter
+        //if it is not, it is left as it is
+        if(isletter(string[i]))
           {
             /*
-             *explaination on how the next line of code works:
-             *lmao i dunno it's black magic
-             *jokes aside, strchr finds which position on the alphabet the
+             *strchr finds which position on the alphabet the
              *letter to be decrypted is located at. Then it substracts
              *the key in order to find where the decrypted letter is
              *in the alphabet
              */
             a = (int)(strchr(alphabet, string[i]) - alphabet);
-            //next for loop is basically "a = a - key;", but
-            //it checks alphabet array's bounds so it doesn't go
-            //below 0 or over 25
-            for(j = 0; j < key; j++)
-              {
-                a--;
-                if(a < 0)
-                  a = 25;
-              }
-            //a = (((int)(strchr(alphabet, string[i]) - alphabet)) - key) % 26;
+
+            // checking the flag provided to see whether we should encrypt or decrypt
+            if(encrypt == 0)
+                a = (a + key) % 26;
+            else if(encrypt == 1)
+                a = (a - key) % 26;
+
             string[i] = alphabet[a];
           }
-        else
-          string[i] = ' ';
       }
-    puts(string);
-    return;
+    return string;
   }
 
-/*
- *encrypt function is basically the same as decrypt, but
- *we're adding the key instead of subtracting it.
- *if you have any doubts, look at the decrypt function
- */
-void encrypt(char *string, int key)
+void usage(char* program_name)
   {
-    int i, j, a, string_lenght;
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-    string_lenght = strlen(string);
-    for(i = 0; i < string_lenght; i++)
-      {
-        if(!isspace(string[i]))
-          {
-            a = (int)(strchr(alphabet, string[i]) - alphabet);
-            for(j = 0; j < key; j++)
-              {
-                a++;
-                if(a > 25)
-                  a = 0;
-              }
-            string[i] = alphabet[a];
-          }
-        else
-          string[i] = ' ';
-      }
-    puts(string);
-    return;
-  }
-
-void usage()
-  {
-    puts("usage: ccypher [options] string_to_decrypt key");
+    printf("usage: %s [options] string_to_decrypt key\n", program_name);
     puts("\toptions:");
     puts("\t  -e, \tencrypt the given string");
     puts("\t  -d, \tdecrypt the given string");
